b1d46d5bc1241cd0fef677a1ac06170edcc30242
kaiserabanto <79958102+kaiserabanto@users.noreply.github.com>
2022-05-10 22:06:53 +0800

    Added github link

--- src/com/ecpe205/Calculator.java
+++ src/com/ecpe205/Calculator.java
@@ -1,26 +1,67 @@
 package com.ecpe205;
 
 public class Calculator {
-    public double sum( double a, double b ) {
-        return a + b;
+    public double power( double a, double b ) {
+        double pow = 1;
+        for(int i=1;i<=b;i++){
+            pow = pow*a;
+        }
+        return pow;
     }
-    public boolean isEven (int value) {
-        return value % 2 == 0;
+    public double factorial(double value) {
+        int fact;
+        if(value>=0){
+        fact=1;
+        for(int i=1;i<=value;i++){
+            fact=fact*i;
+        }
+
+        return fact;
+
+        }
+        else{
+        fact=-1;
+        for(int i=-1;i>=value;i--){
+           fact=fact*i;
+        }
+        if (fact>0) {
+            fact = fact * -1;
+        }
+        return fact;
+        }
     }
-    public boolean isOdd (int value) {
-        return value % 2 == 1;
+    public boolean Palindrome (String word) {
+        int i = 0, g = word.length() - 1;
+
+        // While there are characters to compare
+        while (i < g) {
+
+            // If there is a mismatch
+            if (word.charAt(i) != word.charAt(g))
+                return false;
+
+            i++;
+            g--;
+        }
+
+        return  true;
     }
 
-    //create a method that computes base x power of y
-    //use parameterizedTest methodsource - 5 sets to test
+    public int[] Sort(int[] arr) {
+        int temp;
 
-    //create a method that computes for the factorial of a value
-    //use parameterizedTest- 5 values to test
+        for (int i = 0; i < arr.length; i++) {
+            for (int g = i + 1; g < arr.length; g++) {
+                if (arr[i] > arr[g]) {
+                    temp = arr[i];
+                    arr[i] = arr[g];
+                    arr[g] = temp;
+                }
+            }
+        }
+        return;
+    }
 
-    //create a method that checks if a given string is a palindrome
-    //use parameterizedTest - 5 values to test
 
-    //create a method that accepts an array of integer values, the method then sorts the value in ascending order
-    //** you may choose whatever sort data structure (bubble sort, shell sort,...)
-    //use parameterizedTest methodSource
-}
+    
+    //https://github.com/kaiserabanto/ecpe205-unit-test.git
